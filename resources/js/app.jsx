f3549aa67d4d9b53e4b21decf25fe4aeb13efe1f
jasonchacon <118632152+jasonchacon@users.noreply.github.com>
2025-04-29 20:55:25 -0600

    Update app.jsx

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -1,18 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import DropzoneComponent from './components/DropzoneComponent';
+import React, { useState } from "react";
+import DropzoneComponent from "./DropzoneComponent";
 
 const App = () => {
-    const handleImageUpload = (image) => {
-        console.log('Imagen subida:', image);
+    const [imagen, setImagen] = useState("");
+
+    const handleImageUpload = (imageName) => {
+        setImagen(imageName);
     };
 
     return (
         <div>
             <h1>Subir Imagen</h1>
             <DropzoneComponent onImageUpload={handleImageUpload} />
+            <input type="hidden" name="imagen" value={imagen} />
         </div>
     );
 };
 
-ReactDOM.render(<App />, document.getElementById('app'));
+export default App;
