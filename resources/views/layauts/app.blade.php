b1717a98eb1d9fa6c6ee295e573a0b6621450cc3
jasonchacon <118632152+jasonchacon@users.noreply.github.com>
2025-05-01 14:38:55 -0600

    Update app.blade.php

--- resources/views/layauts/app.blade.php
+++ resources/views/layauts/app.blade.php
@@ -4,16 +4,19 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+    <!-- Estilos adicionales -->
     @stack('styles')
-    @vite('resources/css/app.css')
-    <title>Ulsgram @yield('titulo')</title>
+
+    <!-- Integración con Vite -->
     @vite(['resources/css/app.css', 'resources/js/app.jsx'])
 
+    <title>Ulsgram - @yield('titulo')</title>
     <link rel="icon" href="{{ asset('img/icono.png') }}">
 </head>
 
 <body class="bg-gray-100">
-    <!-- Creamos el encabezado -->
+    <!-- Encabezado -->
     <header class="p-5 border-b bg-white shadow">
         <div class="container mx-auto flex justify-between items-center">
             <a href="{{ route('home') }}" class="text-3xl font-black flex items-center">
@@ -21,7 +24,6 @@
                 <span class="ml-2">Inicio</span>
             </a>
 
-            <!-- Utilizamos la directiva auth para cuando el usuario este autenticado -->
             @auth
                 <nav class="flex gap-2 items-center">
                     <a class="flex items-center gap-2 bg-white border p-2 text-gray-600 rounded text-sm uppercase font-bold cursor-pointer" href="{{ route('posts.create') }}">
@@ -33,7 +35,7 @@
                     </a>
 
                     <a class="font-bold text-gray-600 text-sm" href="{{ route('post.index', auth()->user()->username) }}">
-                        Hola: <span class="font-normal"> {{ auth()->user()->name }} </span>
+                        Hola, <span class="font-normal">{{ auth()->user()->name }}</span>
                     </a>
 
                     <form method="POST" action="{{ route('logout') }}">
@@ -43,7 +45,6 @@
                 </nav>
             @endauth
 
-            <!-- Utilizamos la directiva guest para cuando el usuario no este autenticado -->
             @guest
                 <nav class="flex gap-2 items-center">
                     <a class="font-bold uppercase text-gray-600 text-sm" href="{{ route('login') }}">Login</a>
@@ -53,7 +54,7 @@
         </div>
     </header>
 
-    <!-- Creamos un main, donde pondremos todo el contenido de la página -->
+    <!-- Contenido principal -->
     <main class="container mx-auto mt-10">
         <h2 class="font-black text-center text-3xl mb-10">
             @yield('titulo')
@@ -61,7 +62,7 @@
         @yield('contenido')
     </main>
 
-    <!-- Creamos el píe de página de nuestro sitio web -->
+    <!-- Pie de página -->
     <footer class="mt-10 text-center p-5 text-gray-500 font-bold uppercase">
         Ulsgram - Todos los derechos reservados {{ now()->year }}
     </footer>
