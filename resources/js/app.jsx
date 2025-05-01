b75cb442bb2b829bb531c66d4f2a3f97cb7cc47b
jasonchacon <118632152+jasonchacon@users.noreply.github.com>
2025-05-01 17:12:27 -0600

    jason

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -1,5 +1,10 @@
 import Dropzone from "dropzone";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './app';
 
+const root = ReactDOM.createRoot(document.getElementById('app'));
+root.render(<App />);
 
 const dropzone = new Dropzone(".dropzone", {
 
