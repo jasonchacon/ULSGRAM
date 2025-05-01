ffe76e4f13875eec559f418f1433a159707e79e7
jasonchacon <118632152+jasonchacon@users.noreply.github.com>
2025-05-01 15:01:42 -0600

    Update app.jsx

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -1,20 +1,35 @@
-import React, { useState } from "react";
-import DropzoneComponent from "./components/DropzoneComponent";
+import Dropzone from "dropzone";
 
-function App() {
-    const [imagen, setImagen] = useState("");
 
-    const handleImageUpload = (imageName) => {
-        setImagen(imageName);
-    };
+const dropzone = new Dropzone(".dropzone", {
 
-    return (
-        <div>
-            <h1>Subir Imagen</h1>
-            <DropzoneComponent onImageUpload={handleImageUpload} />
-            <input type="hidden" name="imagen" value={imagen} />
-        </div>
-    );
-}
+    dictDefaultMessage: "Sube aquí tu imagen",
+    acceptedFiles: ".png, .jpg, .jpeg, .gif",
+    addRemoveLinks: true,
+    dictRemoveFile: "Borrar archivo",
+    maxFiles: 1,
+    uploadMultiple: false,
 
-export default App;
+
+    init: function() {
+        if(document.querySelector('[name="imagen"]').value.trim()){
+
+            const imagenPublicada = {}
+            imagenPublicada.size = 1234;
+            imagenPublicada.name = document.querySelector('[name="imagen"]').value;
+
+            this.options.addedfile.call(this, imagenPublicada);
+            this.options.thumbnail.call(this, imagenPublicada, `/uploads/${imagenPublicada.name}`);
+
+            imagenPublicada.previewElement.classList.add('dz-success', 'dz-complete');
+        }
+    },
+});
+
+dropzone.on('success', function(file, response) {
+    document.querySelector('[name="imagen"]').value = response.imagen;
+});
+
+dropzone.on('removedfile', function() {
+    document.querySelector('[name="imagen"]').value = '';
+});
